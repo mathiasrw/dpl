76b26d24e8f79fe92cea8fd45245511a90dc0d25
Mathias Rangel Wulff <m@rawu.dk>
2016-04-01 15:07:35 +0200

    Update surge.rb

--- lib/dpl/provider/surge.rb
+++ lib/dpl/provider/surge.rb
@@ -12,13 +12,13 @@ module DPL
       end
 
       def check_auth
-	if ! context.env['SURGE_TOKEN'] then raise Error, "Please add SURGE_TOKEN in Travis settings (get your token with 'surge token')"
-        if ! context.env['SURGE_LOGIN'] then raise Error, "Please add SURGE_LOGIN in Travis settings (its your email)"
+	#if ! context.env['SURGE_TOKEN'] then raise Error, "Please add SURGE_TOKEN in Travis settings (get your token with 'surge token')"
+        #if ! context.env['SURGE_LOGIN'] then raise Error, "Please add SURGE_LOGIN in Travis settings (its your email)"
       end
 
       def check_app
-      	if ! File.directory?(project) then raise Error, "Please set a valid project folder path in .travis.yml under deploy: project: myPath"
-      	if domain.empty? && ! File.exist?("#{project}/CNAME") then raise Error, "Please set domain in .travis.yml under deploy: project: myDomain (or in a CNAME file in the repo project folder)" 
+      	#if ! File.directory?(project) then raise Error, "Please set a valid project folder path in .travis.yml under deploy: project: myPath"
+      	#if domain.empty? && ! File.exist?("#{project}/CNAME") then raise Error, "Please set domain in .travis.yml under deploy: project: myDomain (or in a CNAME file in the repo project folder)" 
       end
 
       def needs_key?
