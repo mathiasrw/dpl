73f450041dfa3404084b43f8813e7c72e2ef026e
Mathias Rangel Wulff <m@rawu.dk>
2016-04-01 15:47:03 +0200

    Update surge.rb

--- lib/dpl/provider/surge.rb
+++ lib/dpl/provider/surge.rb
@@ -12,13 +12,13 @@ module DPL
       end
 
       def check_auth
-	#if ! context.env['SURGE_TOKEN'] then raise Error, "Please add SURGE_TOKEN in Travis settings (get your token with 'surge token')"
-        #if ! context.env['SURGE_LOGIN'] then raise Error, "Please add SURGE_LOGIN in Travis settings (its your email)"
+	if ! context.env['SURGE_TOKEN'] then raise Error, "Please add SURGE_TOKEN in Travis settings (get your token with 'surge token')" end
+        if ! context.env['SURGE_LOGIN'] then raise Error, "Please add SURGE_LOGIN in Travis settings (its your email)" end
       end
 
       def check_app
-      	#if ! File.directory?(project) then raise Error, "Please set a valid project folder path in .travis.yml under deploy: project: myPath"
-      	#if domain.empty? && ! File.exist?("#{project}/CNAME") then raise Error, "Please set domain in .travis.yml under deploy: project: myDomain (or in a CNAME file in the repo project folder)" 
+      	if ! File.directory?(project) then raise Error, "Please set a valid project folder path in .travis.yml under deploy: project: myPath" end
+      	if domain.empty? && ! File.exist?("#{project}/CNAME") then raise Error, "Please set domain in .travis.yml under deploy: project: myDomain (or in a CNAME file in the repo project folder)" end
       end
 
       def needs_key?
@@ -26,10 +26,7 @@ module DPL
       end
 
       def push_app
-      	context.shell "echo a " + Dir.pwd
-      	context.shell "cd .."
-      	context.shell "echo b " + Dir.pwd
-        #context.shell "surge #{project} #{domain}"
+        context.shell "surge #{project} #{domain}"
       end
     end
   end
